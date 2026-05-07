3732eccb271fc4e74f8a115ca5c29182d8729c84
Iesmar <vincent.peschar@gmail.com>
2026-05-07 14:57:54 +0200

    trying to deploy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-import os, shutil
+import os, shutil, sys
 
 from htmlblocks import markdown_to_html_node, extract_title
 from textnode import TextNode
 
 
 def main():
+
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     # first check if path exist - yes->Delete - No->Create
     if os.path.exists("./public"):
         print("cleaned up old files!")
@@ -16,38 +22,38 @@ def main():
     root_content_path = "./content/"
     root_template = "./template.html"
     root_public = "./public/"
-    generate_website(root_content_path, root_template, root_public)
+    generate_website(root_content_path, root_template, root_public, basepath)
     copy_files(path)
 
     #generate_page("./content/index.md","./template.html","./public/index.html")
 
     
-def generate_website(content_root, template_root, dest_root):
+def generate_website(content_root, template_root, dest_root, basepath):
 
     print(f"I AM IN THE FOLLOWING {content_root}")
     files_to_convert = os.listdir(f"{content_root}")
     print("\n\nThese files need to be converted/coppied!")
     print(files_to_convert)
     for item in files_to_convert:
-            new_content_root = f"{content_root}{item}" #
+            new_content_root = f"{content_root}{item}"
             new_dest_root = f"{dest_root}{item}/"
             #if files_to_convert == []:
                 #print("empty folder!!!!!")
                 #break
-            if os.path.isfile(f"{new_content_root}"):
-                generate_page(f"{new_content_root}", template_root, f"{str(new_dest_root[:-4])}.html")
-                print(f"converted {new_content_root} into {str(new_dest_root[:-4])}.html")    
+            if os.path.isfile(f"{content_root}{item}"):
+                generate_page(f"{content_root}{item}", template_root, f"{dest_root}{str(item)[:-3]}.html", basepath)
+                print(f"converted {content_root}{item} into {dest_root}{str(item)[:-3]}.html")    
             elif os.path.isfile(f"{new_content_root}") == False:
                 #content_root += f"{item}/"
                 #dest_root += f"{item}/"
                 os.makedirs(f"{new_dest_root}", exist_ok=True)
                 #print(f"created dir {dest_root}") 
                 #print(f"moving into dir {content_root}")
-                generate_website(f"{new_content_root}/", template_root, new_dest_root)
+                generate_website(f"{new_content_root}/", template_root, new_dest_root, basepath)
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"\nGenerating page from {from_path}\nto {dest_path} using {template_path}\n")
     
     with open(from_path) as f:
@@ -61,16 +67,13 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
 
     template = template.replace("{{ Title }}", title)
-    final_page = template.replace("{{ Content }}", html_page)
+    template = template.replace("{{ Content }}", html_page)
+    template = template.replace('href="/', f'href="{basepath}')
+    final_page = template.replace('src="/', f'src="{basepath}')
+
 
     with open(f"{dest_path}", "w") as f:
         f.write(final_page)
-
-
-
-
-
-
 
 def copy_files(path):
         files_to_copy = os.listdir(f"./static/{path}")
